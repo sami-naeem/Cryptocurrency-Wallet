929cb15fd2de6c2bf1eaefd970ab34290356600b
samimuha17 <samimuha17@gmail.com>
2023-01-31 20:57:30 -0600

    final files

--- Instructions/Starter-Code/krypto_jobs.py
+++ Instructions/Starter-Code/krypto_jobs.py
@@ -80,7 +80,12 @@ w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 # @TODO:
 # From `crypto_wallet.py import the functions generate_account, get_balance,
 #  and send_transaction
-# YOUR CODE HERE
+
+from crypto_wallet import generate_account
+
+from crypto_wallet import get_balance
+
+from crypto_wallet import send_transaction
 
 ################################################################################
 # KryptoJobs2Go Candidate Information
@@ -156,7 +161,8 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-# YOUR CODE HERE
+
+account = generate_account()
 
 ##########################################
 
@@ -172,7 +178,12 @@ st.sidebar.write(account.address)
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-# YOUR CODE HERE
+
+balance = get_balance(account.address)
+
+st.sidebar.markdown("## Your Ether Balance")
+st.sidebar.markdown(balance)
+st.sidebar.markdown("-------------")
 
 ##########################################
 
@@ -294,7 +305,9 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    # YOUR CODE HERE
+    
+    transaction_hash = send_transaction(account, candidate_address, wage)
+    hash = transaction_hash
 
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
